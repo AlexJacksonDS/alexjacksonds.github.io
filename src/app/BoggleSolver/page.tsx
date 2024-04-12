98792f5039327ccf016d44240174ab284e2b5153
Alex Jackson <alexander.jackson@dorsetsoftware.com>
2024-04-12 11:30:22 +0100

    BoggleSolver corrections

--- src/app/BoggleSolver/page.tsx
+++ src/app/BoggleSolver/page.tsx
@@ -2,10 +2,13 @@
 
 import { ChangeEvent, useState } from "react";
 import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import "./BoggleSolver.scss";
 
 export default function BoggleSolverPage() {
   const [file, setFile] = useState<File | undefined>(undefined);
   const [wordGroups, setWordGroups] = useState<WordGroups[]>([]);
+  const [dice, setDice] = useState<string[]>([]);
+  const [diceString, setDiceString] = useState("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -22,25 +25,67 @@ export default function BoggleSolverPage() {
       });
 
       if (response.ok) {
-        const res: WordGroups[] = await response.json();
-        setWordGroups(res);
+        const res: BoggleResults = await response.json();
+        setWordGroups(res.results);
+        setDice(res.readGrid);
+        setDiceString(res.readGrid.map((d) => (d.toUpperCase() === "QU" ? "Q" : d)).join(""));
       }
     }
+  };
+
+  const correctGrid = async () => {
+    if (diceString) {
+      const data = { boggleString: diceString };
+      const response = await fetch("https://ajj-sig-test.azurewebsites.net/bogglestring", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const res: BoggleResults = await response.json();
+        setWordGroups(res.results);
+        setDice(res.readGrid);
+        setDiceString(res.readGrid.map((d) => (d.toUpperCase() === "QU" ? "Q" : d)).join(""));
+      }
+    }
+  };
+
+  const handleSelectChange = (index: number, value: string) => {
+    const newDice = [...dice];
+    newDice[index] = value === "Q" ? "Qu" : value;
+    setDice(newDice);
+    setDiceString(newDice.map((d) => (d.toUpperCase() === "QU" ? "Q" : d)).join(""));
   };
 
   return (
     <>
       <Container>
         <Row className="pt-4">
-          <Col>
+          <Col xs={4}>
             <input className="form-control" type="file" onChange={handleChange} />
           </Col>
-          <Col>
+          <Col xs={2}>
             <button onClick={uploadImage} className="btn btn-primary">
               Upload
             </button>
           </Col>
+          <Col xs={2}>
+            <div className="dice-grid">
+              {dice.map((x, i) => (
+                <div key={i}>
+                  <CharSelect currentValue={x} index={i} handleChange={handleSelectChange} />
+                </div>
+              ))}
+            </div>
+          </Col>
+          <Col xs={2}>
+            <button onClick={correctGrid} className="btn btn-primary">
+              Submit Correction
+            </button>
+          </Col>
         </Row>
+
         <Row xs={1} md={2} lg={4} className="g-4 pt-4">
           {wordGroups.map((x) => (
             <Col key={x.length}>
@@ -60,6 +105,38 @@ export default function BoggleSolverPage() {
       </Container>
     </>
   );
+}
+
+function CharSelect({
+  index,
+  currentValue,
+  handleChange,
+}: {
+  index: number;
+  currentValue: string;
+  handleChange: (index: number, value: string) => void;
+}) {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    handleChange(index, value);
+  };
+
+  return (
+    <select onChange={onChange} value={currentValue}>
+      {chars.map((c) => (
+        <option key={c} value={c}>
+          {c === "Q" ? "Qu" : c}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+interface BoggleResults {
+  results: WordGroups[];
+  readGrid: string[];
 }
 
 interface WordGroups {
