4fcc4a00d4b50df10a77e8e160fcf11d12931fc6
Alex Jackson <alexander.jackson@dorsetsoftware.com>
2024-05-10 11:26:47 +0100

    Ice Puzzle

--- src/components/IcePuzzle/IcePuzzle/IcePuzzle.tsx
+++ src/components/IcePuzzle/IcePuzzle/IcePuzzle.tsx
@@ -1,39 +1,85 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 import "./IcePuzzle.scss";
-
-enum Tile {
-  Ice,
-  Rock,
-  Goal,
-  Start,
-}
+import { IcePuzzleGenerator, Tile } from "@/services/icePuzzle.service";
+import RangeSlider from "react-bootstrap-range-slider";
 
 export default function IcePuzzle() {
-  const [map, setMap] = useState<Tile[][]>(defaultBoard);
-  const currentI = useRef(13);
-  const currentJ = useRef(14);
+  const [generator, _] = useState(new IcePuzzleGenerator());
+  const map = useRef<Tile[][]>([]);
+  const scale = useRef(50);
+
+  const [width, setWidth] = useState(10);
+  const [height, setHeight] = useState(10);
+  const [bypassTimeout, setBypassTimeout] = useState(false);
+
+  const rawSize = Math.floor(500 / Math.max(width, height));
+  scale.current = rawSize > 50 ? 50 : rawSize;
+
+  const [renderMap, setRenderMap] = useState(false);
+
+  const currentI = useRef(0);
+  const currentJ = useRef(0);
+  const startI = useRef(0);
+  const startJ = useRef(0);
   const [isInit, setIsInit] = useState(false);
-  const [needsRerender, setNeedsRerender] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [rect, setRect] = useState<DOMRect | undefined>(undefined);
+
+  const [showSolution, setShowSolution] = useState(false);
+  const [solution, setSolution] = useState("");
+  const [generationStats, setGenerationStats] = useState("");
 
   useEffect(() => {
+    if (!rect && renderMap) {
+      setRect(document.getElementById("ice-puzzle")?.getBoundingClientRect());
+    }
+    if (rect && renderMap) {
+      drawCurrentPlayerLocation(currentJ.current * scale.current, currentI.current * scale.current);
+    }
     if (!isInit) {
       document.addEventListener("keyup", handleKeyPress);
+
       setIsInit(true);
 
       return () => {
         document.removeEventListener("keydown", handleKeyPress);
       };
     }
-  });
+  }, [rect, setRect, renderMap]);
 
-  useEffect(() => setNeedsRerender(false));
+  function drawCurrentPlayerLocation(x: number, y: number) {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      if (context) {
+        context.reset();
+        context.beginPath();
+        context.arc(
+          x + Math.floor(scale.current / 2),
+          y + Math.floor(scale.current / 2),
+          Math.floor(scale.current / 4),
+          0,
+          2 * Math.PI
+        );
+        context.fillStyle = "red";
+        context.strokeStyle = "red";
+        context.fill();
+        context.stroke();
+      }
+    }
+  }
 
   function handleKeyPress(e: KeyboardEvent) {
-    switch (e.key) {
+    handleKey(e.key);
+  }
+
+  function handleKey(key: string) {
+    switch (key) {
       case "ArrowLeft":
         movePlayer(0, -1);
         break;
@@ -49,296 +95,184 @@ export default function IcePuzzle() {
     }
   }
 
-  function movePlayer(iMod: number, jMod: number) {
+  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+  async function movePlayer(iMod: number, jMod: number) {
+    if (map.current.length === 0) return;
     let isRock = false;
+    let isStart = false;
+    startI.current = currentI.current;
+    startJ.current = currentJ.current;
     let i = currentI.current + iMod;
     let j = currentJ.current + jMod;
-    while (!isRock) {
-      if (map[i][j] === Tile.Rock) {
+    while (!isRock && !isStart) {
+      if (map.current[i][j] === Tile.Rock) {
         isRock = true;
+      } else if (map.current[i][j] === Tile.Start || map.current[i][j] === Tile.Goal) {
+        isStart = true;
+        drawCurrentPlayerLocation(j * scale.current, i * scale.current);
       } else {
+        drawCurrentPlayerLocation(
+          j * scale.current - Math.floor((3 * scale.current) / 4) * jMod,
+          i * scale.current - Math.floor((3 * scale.current) / 4) * iMod
+        );
+        await delay(25);
+        drawCurrentPlayerLocation(
+          j * scale.current - Math.floor(scale.current / 2) * jMod,
+          i * scale.current - Math.floor(scale.current / 2) * iMod
+        );
+        await delay(25);
+        drawCurrentPlayerLocation(
+          j * scale.current - Math.floor((1 * scale.current) / 4) * jMod,
+          i * scale.current - Math.floor((1 * scale.current) / 4) * iMod
+        );
+        await delay(25);
+        drawCurrentPlayerLocation(j * scale.current, i * scale.current);
         i += iMod;
         j += jMod;
+        await delay(25);
       }
     }
-    i -= iMod;
-    j -= jMod;
+    if (isRock) {
+      i -= iMod;
+      j -= jMod;
+    }
     currentI.current = i;
     currentJ.current = j;
-    setNeedsRerender(true);
+  }
+
+  async function generateBoard() {
+    const game = generator.Generate(width, height, bypassTimeout);
+    map.current = game.board;
+    currentJ.current = game.start.x;
+    currentI.current = game.start.y;
+    setSolution(game.solutionString);
+    setGenerationStats("Map generated in " + game.timeTaken + "ms in " + game.attempts + " attempts");
+    drawCurrentPlayerLocation(currentJ.current * scale.current, currentI.current * scale.current);
+    setRect(undefined);
+    setRenderMap(true);
+  }
+
+  function Up() {
+    movePlayer(-1, 0);
   }
 
   return (
     <>
-      <Container>Arrow keys to move</Container>
       <Container>
-        {map.map((r, i) => (
-          <Row key={i}>
-            {r.map((c, j) => (
-              <Col key={j} className="ice-col">
-                <div className={"tile " + Object.keys(Tile)[Object.values(Tile).indexOf(c)].toLowerCase()}>
-                  {i === currentI.current && j === currentJ.current ? <div className="player"></div> : null}
-                </div>
-              </Col>
-            ))}
-          </Row>
-        ))}
+        <Form>
+          <Form.Group as={Row}>
+            <Col>
+              <Form.Group>
+                <Form.Label>Width</Form.Label>
+                <RangeSlider
+                  value={width}
+                  onChange={(e) => {
+                    setWidth(parseInt(e.target.value));
+                    setRenderMap(false);
+                  }}
+                  min={10}
+                  max={20}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Label>Height</Form.Label>
+                <RangeSlider
+                  value={height}
+                  onChange={(e) => {
+                    setHeight(parseInt(e.target.value));
+                    setRenderMap(false);
+                  }}
+                  min={10}
+                  max={20}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  onChange={() => setBypassTimeout(!bypassTimeout)}
+                  checked={bypassTimeout}
+                  label="Bypass timeout"
+                />
+              </Form.Group>
+            </Col>
+          </Form.Group>
+          <Button onClick={generateBoard}>Generate Map</Button>
+          {width >= 15 && height >= 15 ? (
+            <p>
+              At this size performance of generation will be impacted and map may be very simple/impossible. To bypass
+              the performance timeout of 30 seconds select the checkbox above.
+            </p>
+          ) : null}
+        </Form>
       </Container>
+      {renderMap ? (
+        <>
+          <Container>
+            <p>{generationStats}</p>
+          </Container>
+          <Container>Arrow keys or buttons to move</Container>
+          <Container>
+            <Row>
+              <Col xs={1}></Col>
+              <Col xs={1}>
+                <Button onClick={() => handleKey("ArrowUp" )}>Up</Button>
+              </Col>
+              <Col xs={1}></Col>
+            </Row>
+            <Row>
+              <Col xs={1}><Button onClick={() => handleKey("ArrowLeft" )}>Left</Button></Col>
+              <Col xs={1}><Button onClick={() => handleKey("ArrowDown" )}>Down</Button></Col>
+              <Col xs={1}><Button onClick={() => handleKey("ArrowRight" )}>Right</Button></Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col lg={2}>
+                <p>{showSolution ? solution : "***********"}</p>
+              </Col>
+              <Col lg={2}>
+                <Button onClick={() => setShowSolution(!showSolution)}>
+                  {showSolution ? "Hide" : "Show"} Solution
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+          <Container id="ice-puzzle">
+            {map.current.map((r, i) => (
+              <Row key={i}>
+                {r.map((c, j) => (
+                  <Col
+                    key={j}
+                    style={{
+                      width: scale.current + "px",
+                      maxWidth: scale.current + "px",
+                    }}
+                  >
+                    <div
+                      className={"tile " + Object.keys(Tile)[Object.values(Tile).indexOf(c)].toLowerCase()}
+                      style={{ width: scale.current + "px" }}
+                    ></div>
+                  </Col>
+                ))}
+              </Row>
+            ))}
+          </Container>
+        </>
+      ) : null}
+      {renderMap && rect && map.current.length > 0 ? (
+        <canvas
+          ref={canvasRef}
+          width={map.current[0].length * scale.current}
+          height={map.current.length * scale.current}
+          id="player-canvas"
+          style={{ zIndex: 500, position: "fixed", top: rect.top, left: rect.left }}
+        />
+      ) : null}
     </>
   );
 }
-
-const defaultBoard = [
-  [
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Goal,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Goal,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Ice,
-    Tile.Rock,
-  ],
-  [
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Rock,
-    Tile.Start,
-    Tile.Rock,
-  ],
-];
---
+++ src/services/icePuzzle.service.ts
@@ -0,0 +1,249 @@
+interface Point {
+  x: number;
+  y: number;
+}
+
+export enum Tile {
+  Ice,
+  Rock,
+  Goal,
+  Start,
+}
+
+export class IcePuzzleGenerator {
+  width: number;
+  height: number;
+  directionIndex: number;
+  board: number[][];
+  pathDirectionIndices: number[];
+  start: Point;
+  end: Point;
+  current: Point;
+
+  constructor() {
+    this.width = 0;
+    this.height = 0;
+    this.directionIndex = 0;
+    this.board = [];
+    this.pathDirectionIndices = [];
+    this.start = { x: 0, y: 0 };
+    this.end = { x: 0, y: 0 };
+    this.current = { x: 0, y: 0 };
+  }
+
+  Generate(width: number, height: number, bypassTimeout: boolean) {
+    const start = performance.now();
+    this.width = width;
+    this.height = height;
+
+    let o = false;
+    let attempts = 0;
+    while (!o && (performance.now() - start < 30000 || bypassTimeout)) {
+      this.board = Array(this.width + 2)
+        .fill(null)
+        .map(() => Array(this.height + 2).fill(0));
+
+      for (let i = 0; i < this.width + 2; i++)
+        for (let j = 0; j < this.height + 2; j++)
+          if (i === 0 || i === this.width + 1 || j === 0 || j === this.height + 1) this.board[i][j] = 2;
+
+      this.start = { x: 0, y: 0 };
+      let randomPerimeterDistance = Math.floor(Math.random() * (this.width * 2 + this.height * 2));
+      if (randomPerimeterDistance < this.width * 2) {
+        this.start.x = (randomPerimeterDistance % this.width) + 1;
+        this.start.y = randomPerimeterDistance < this.width ? 0 : this.height + 1;
+      } else {
+        this.start.y = ((randomPerimeterDistance - this.width * 2) % this.height) + 1;
+        this.start.x = randomPerimeterDistance - this.width * 2 < this.height ? 0 : this.width + 1;
+      }
+      if (this.start.x < 1) {
+        this.directionIndex = 3;
+      } else if (this.start.x > this.width) {
+        this.directionIndex = 1;
+      } else if (this.start.y < 1) {
+        this.directionIndex = 2;
+      } else if (this.start.y > this.height) {
+        this.directionIndex = 0;
+      }
+
+      this.board[this.start.x][this.start.y] = 0;
+      this.current = { ...this.start };
+      this.pathDirectionIndices = [];
+      this.pathDirectionIndices.push(this.directionIndex);
+
+      for (let i = 0; i++ < Math.min(this.width, this.height) - 1; ) {
+        if (!this.N()) break;
+      }
+      o =
+        this.pathDirectionIndices.length >= Math.min(this.width, this.height) - 1 &&
+        this.F() &&
+        this.V() === this.pathDirectionIndices.length;
+      attempts++;
+    }
+
+    const tempBoard: Tile[][] = Array(this.width + 2)
+      .fill(null)
+      .map(() => Array(this.height + 2).fill(Tile.Rock));
+
+    for (let j = 1; j < this.height + 1; j++) {
+      for (let i = 1; i < this.width + 1; i++) {
+        tempBoard[i][j] = this.board[i][j] > 0 ? Tile.Rock : Tile.Ice;
+      }
+    }
+    
+    const finalBoard = tempBoard[0]
+      .map((_val, index) => tempBoard.map((row) => row[index]).reverse())
+      .map(function (arr) {
+        return arr.reverse();
+      });
+
+    finalBoard[this.start.y][this.start.x] = Tile.Start;
+
+    finalBoard[this.end.y][this.end.x] = Tile.Goal;
+
+    const dirs = ["U", "L", "D", "R"];
+    const solutionString = this.pathDirectionIndices.map((e) => dirs[e]).join("");
+    return {
+      solutionString,
+      board: finalBoard,
+      start: this.start,
+      timeTaken: (performance.now() - start).toFixed(2),
+      attempts,
+    };
+  }
+
+  F(): boolean {
+    let c = 0;
+    while (this.C() < 1 && c++ < 10) {
+      if (!this.N()) return false;
+    }
+    return true;
+  }
+
+  C() {
+    let d = this.directionIndex < 2 ? -1 : 1;
+    if (this.directionIndex % 2 < 1) {
+      let y = this.current.y;
+      while (y > 0 && y < this.height + 1) {
+        y += d;
+        if (this.board[this.current.x][y] === 1) {
+          return 0;
+        }
+      }
+      this.end = { x: this.current.x, y };
+    } else {
+      let x = this.current.x;
+      while (x > 0 && x < this.width + 1) {
+        x += d;
+        if (this.board[x][this.current.y] === 1) {
+          return 0;
+        }
+      }
+      this.end = { x, y: this.current.y };
+    }
+    this.board[this.end.x][this.end.y] = 0;
+    return 1;
+  }
+
+  V() {
+    if (this.start.x - this.end.x + (this.start.y - this.end.y) < 2) {
+      return 0;
+    }
+    let pointQueue: Point[] = [];
+    let d: number[] = [];
+    this.board[this.start.x][this.start.y] = -2;
+
+    pointQueue.push(this.start);
+    d.push(0);
+    while (pointQueue.length > 0) {
+      let t = pointQueue.shift()!;
+      let h = d.shift()!;
+      if (t.x === this.end.x && t.y === this.end.y) {
+        return h;
+      }
+      for (let i = 0; i < 4; i++) {
+        let n = this.S(t, i < 2 ? 0 : 1, i % 2 < 1 ? -1 : 1, 99, false);
+        if (this.board[n.x][n.y] === -2) continue;
+        this.board[n.x][n.y] = -2;
+        pointQueue.push(n);
+        d.push(h + 1);
+      }
+    }
+    return 0;
+  }
+
+  N(): boolean {
+    let d: -1 | 1 = this.directionIndex < 2 ? -1 : 1;
+
+    const q = this.S(
+      this.current,
+      this.directionIndex,
+      d,
+      Math.floor((Math.random() * (this.directionIndex % 2 < 1 ? this.height : this.width)) / 2) + 2,
+      true
+    );
+    if (
+      q.x < 1 ||
+      q.y < 1 ||
+      q.x > this.width ||
+      q.y > this.height ||
+      (q.x === this.current.x && q.y === this.current.y) ||
+      this.board[q.x][q.y] != 0
+    ) {
+      return false;
+    }
+    let x = q.x;
+    let y = q.y;
+    if (this.directionIndex % 2 < 1) {
+      y += d;
+    } else {
+      x += d;
+    }
+    if (this.board[x][y] < 0) {
+      return false;
+    }
+    this.board[q.x][q.y] = -1;
+    this.board[x][y] = 1;
+    let f = Math.floor(Math.random() * 2) < 1 ? -1 : 1;
+    this.directionIndex = this.directionIndex % 2 < 1 ? (f < 0 ? 1 : 3) : (this.directionIndex = f < 0 ? 0 : 2);
+    this.current = q;
+
+    this.pathDirectionIndices.push(this.directionIndex);
+    return true;
+  }
+
+  S(currentPoint: Point, direction: number, d: -1 | 1, q: number, s: boolean): Point {
+    let i = 1,
+      x = currentPoint.x,
+      y = currentPoint.y;
+    for (; i <= q; i++) {
+      if (direction % 2 < 1) {
+        y = currentPoint.y + i * d;
+      } else {
+        x = currentPoint.x + i * d;
+      }
+      if (this.end.x === x && this.end.y === y) {
+        return this.end;
+      }
+      if (y < 0 || y > this.height + 1 || x < 0 || x > this.width + 1) {
+        return currentPoint;
+      }
+      if (s && this.board[x][y] < 1) {
+        this.board[x][y] = -1;
+      }
+      if (this.board[x][y] > 0) {
+        if (direction % 2 < 1) {
+          y -= d;
+        } else {
+          x -= d;
+        }
+        return { x, y };
+      }
+    }
+    if (direction % 2 < 1) {
+      return { x: currentPoint.x, y: currentPoint.y + i * d };
+    } else {
+      return { x: currentPoint.x + i * d, y: currentPoint.y };
+    }
+  }
+}
