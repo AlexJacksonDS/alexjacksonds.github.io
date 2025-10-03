1ab74f10e6b8272b92873374f5daed3e2877e3af
Alex Jackson <alexander.jackson@dorsetsoftware.com>
2025-10-03 16:44:55 +0100

    Path Clicks

--- src/components/Transamerica/Hex/Hex.tsx
+++ src/components/Transamerica/Hex/Hex.tsx
@@ -1,21 +1,22 @@
-import { CSSProperties } from "react";
+import { CSSProperties, MouseEvent } from "react";
 import "./Hex.scss";
 import { randEnumValue } from "@/helpers/enumHelper";
 
 export class HexConnections {
-  ot: ConnectionState;
-  otl: ConnectionState;
-  otr: ConnectionState;
-  ob: ConnectionState;
-  obl: ConnectionState;
-  obr: ConnectionState;
-  il: ConnectionState;
-  itl: ConnectionState;
-  itr: ConnectionState;
-  ir: ConnectionState;
-  ibr: ConnectionState;
-  ibl: ConnectionState;
+//   ot: ConnectionState;
+//   otl: ConnectionState;
+//   otr: ConnectionState;
+//   ob: ConnectionState;
+//   obl: ConnectionState;
+//   obr: ConnectionState;
+//   il: ConnectionState;
+//   itl: ConnectionState;
+//   itr: ConnectionState;
+//   ir: ConnectionState;
+//   ibr: ConnectionState;
+//   ibl: ConnectionState;
   cityPlace?: CityPlace;
+  c: Map<string, ConnectionState>;
 
   constructor(
     random: boolean,
@@ -34,31 +35,46 @@ export class HexConnections {
     cityplace?: CityPlace
   ) {
     if (!random && ot && otl && otr && ob && obl && obr && il && itl && itr && ir && ibr && ibl) {
-      this.ot = ot;
-      this.otl = otl;
-      this.otr = otr;
-      this.ob = ob;
-      this.obl = obl;
-      this.obr = obr;
-      this.il = il;
-      this.itl = itl;
-      this.itr = itr;
-      this.ir = ir;
-      this.ibr = ibr;
-      this.ibl = ibl;
+    //   this.ot = ot;
+    //   this.otl = otl;
+    //   this.otr = otr;
+    //   this.ob = ob;
+    //   this.obl = obl;
+    //   this.obr = obr;
+    //   this.il = il;
+    //   this.itl = itl;
+    //   this.itr = itr;
+    //   this.ir = ir;
+    //   this.ibr = ibr;
+    //   this.ibl = ibl;
+      this.c = new Map<string, ConnectionState>([
+        ["ot", ot],
+        ["otl", otl],
+        ["otr", otr],
+        ["ob", ob],
+        ["obl", obl],
+        ["obr", obr],
+        ["il", il],
+        ["itl", itl],
+        ["itr", itr],
+        ["ir", ir],
+        ["ibr", ibr],
+        ["ibl", ibl],
+      ]);
     } else {
-      this.ot = randEnumValue(ConnectionState);
-      this.otl = randEnumValue(ConnectionState);
-      this.otr = randEnumValue(ConnectionState);
-      this.ob = randEnumValue(ConnectionState);
-      this.obl = randEnumValue(ConnectionState);
-      this.obr = randEnumValue(ConnectionState);
-      this.il = randEnumValue(ConnectionState);
-      this.itl = randEnumValue(ConnectionState);
-      this.itr = randEnumValue(ConnectionState);
-      this.ir = randEnumValue(ConnectionState);
-      this.ibr = randEnumValue(ConnectionState);
-      this.ibl = randEnumValue(ConnectionState);
+    //   this.ot = randEnumValue(ConnectionState);
+    //   this.otl = randEnumValue(ConnectionState);
+    //   this.otr = randEnumValue(ConnectionState);
+    //   this.ob = randEnumValue(ConnectionState);
+    //   this.obl = randEnumValue(ConnectionState);
+    //   this.obr = randEnumValue(ConnectionState);
+    //   this.il = randEnumValue(ConnectionState);
+    //   this.itl = randEnumValue(ConnectionState);
+    //   this.itr = randEnumValue(ConnectionState);
+    //   this.ir = randEnumValue(ConnectionState);
+    //   this.ibr = randEnumValue(ConnectionState);
+    //   this.ibl = randEnumValue(ConnectionState);
+      this.c = new Map<string, ConnectionState>([]);
     }
     this.cityPlace = cityplace;
   }
@@ -102,6 +118,7 @@ export default function Hex({
   rowOffset,
   columnOffset,
   radius = 100,
+  onClick,
 }: {
   connections: HexConnections;
   row: number;
@@ -110,6 +127,7 @@ export default function Hex({
   columnOffset: number;
   edgeWidth?: number;
   radius?: number;
+  onClick: (e: MouseEvent, i: number, j: number, seg: string) => void;
 }) {
   const displayColumn = column + columnOffset + (row % 2 == 0 ? 0.5 : 0);
   const innerRadius = (Math.sqrt(3) * radius) / 2;
@@ -127,7 +145,7 @@ export default function Hex({
     height: `${radius * 2}px`,
   };
 
-  function getColourClass(connectionState: ConnectionState) {
+  function getColourClass(connectionState: ConnectionState | undefined) {
     switch (connectionState) {
       case ConnectionState.Connected:
         return "connected";
@@ -136,6 +154,7 @@ export default function Hex({
       case ConnectionState.UnconnectedTwo:
         return "unconnectedTwo";
       case ConnectionState.Invalid:
+      default:
         return "invalid";
     }
   }
@@ -143,49 +162,69 @@ export default function Hex({
   return (
     <div className="hex" style={hexStyle}>
       <div className="clipHex" style={clipHexStyle}>
-        <div className={"clipLine sideLength mid " + getColourClass(connections["il"])}></div>
         <div
-          className={"clipLine sideLength l1 " + getColourClass(connections["ot"])}
+          className={"clipLine sideLength mid " + getColourClass(connections.c.get("il"))}
+          onClick={(e) => onClick(e, row, column, "il")}
+        ></div>
+        <div
+          className={"clipLine sideLength l1 " + getColourClass(connections.c.get("ot"))}
           style={{ top: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "ot")}
         ></div>
         <div
-          className={"clipLine sideLength l1 " + getColourClass(connections["ob"])}
+          className={"clipLine sideLength l1 " + getColourClass(connections.c.get("ob"))}
           style={{ bottom: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "ob")}
         ></div>
-        <div className={"clipLine sideLength l2 mid " + getColourClass(connections["ir"])}></div>
         <div
-          className={"clipLine narrow backDiag l1 " + getColourClass(connections["itl"])}
+          className={"clipLine sideLength l2 mid " + getColourClass(connections.c.get("ir"))}
+          onClick={(e) => onClick(e, row, column, "ir")}
+        ></div>
+        <div
+          className={"clipLine narrow backDiag l1 " + getColourClass(connections.c.get("itl"))}
           style={{ height: `${innerRadius + 1}px`, top: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "itl")}
         ></div>
         <div
-          className={"clipLine narrow diag " + getColourClass(connections["otl"])}
+          className={"clipLine narrow diag " + getColourClass(connections.c.get("otl"))}
           style={{ height: `${innerRadius + 2}px`, top: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "otl")}
         ></div>
         <div
-          className={"clipLine narrow backDiag l3 " + getColourClass(connections["otr"])}
+          className={"clipLine narrow backDiag l3 " + getColourClass(connections.c.get("otr"))}
           style={{ height: `${innerRadius + 2}px`, top: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "otr")}
         ></div>
         <div
-          className={"clipLine narrow diag l2 " + getColourClass(connections["itr"])}
+          className={"clipLine narrow diag l2 " + getColourClass(connections.c.get("itr"))}
           style={{ height: `${innerRadius + 1}px`, top: `${radius - innerRadius - 1}px` }}
+          onClick={(e) => onClick(e, row, column, "itr")}
         ></div>
         <div
-          className={"clipLine narrow diag bottom l1 " + getColourClass(connections["ibl"])}
+          className={"clipLine narrow diag bottom l1 " + getColourClass(connections.c.get("ibl"))}
           style={{ height: `${innerRadius + 1}px` }}
+          onClick={(e) => onClick(e, row, column, "ibl")}
         ></div>
         <div
-          className={"clipLine narrow backDiag bottom l2 " + getColourClass(connections["ibr"])}
+          className={"clipLine narrow backDiag bottom l2 " + getColourClass(connections.c.get("ibr"))}
           style={{ height: `${innerRadius + 1}px` }}
+          onClick={(e) => onClick(e, row, column, "ibr")}
         ></div>
         <div
-          className={"clipLine narrow bottom backDiag " + getColourClass(connections["obl"])}
+          className={"clipLine narrow bottom backDiag " + getColourClass(connections.c.get("obl"))}
           style={{ height: `${innerRadius + 1}px` }}
+          onClick={(e) => onClick(e, row, column, "obl")}
         ></div>
         <div
-          className={"clipLine narrow diag bottom l3 " + getColourClass(connections["obr"])}
+          className={"clipLine narrow diag bottom l3 " + getColourClass(connections.c.get("obr"))}
           style={{ height: `${innerRadius + 1}px` }}
+          onClick={(e) => onClick(e, row, column, "obr")}
         ></div>
-        {connections.cityPlace ? <div className={"miniBlock " + connections.cityPlace.colour + " " + connections.cityPlace.place}>{connections.cityPlace.city}</div> : null}
+        {connections.cityPlace ? (
+          <div className={"cityCircle " + connections.cityPlace.colour + " " + connections.cityPlace.place}>
+            {connections.cityPlace.city}
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- src/components/Transamerica/Transamerica.tsx
+++ src/components/Transamerica/Transamerica.tsx
@@ -1,197 +1,209 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, MouseEvent, useRef } from "react";
 import { Button, Container } from "react-bootstrap";
 import "./Transamerica.scss";
 import _ from "lodash";
-import Hex, { HexConnections } from "./Hex/Hex";
+import Hex, { ConnectionState } from "./Hex/Hex";
 import { blankBoard, Tile } from "@/services/transamerica.service";
 
 export default function Transamerica() {
-    const [rowOffset, setRowOffset] = useState(0);
-    const [colOffset, setColOffset] = useState(0);
-    const [height, setheight] = useState(0);
-    const [width, setWidth] = useState(0);
+  const [rowOffset, setRowOffset] = useState(0);
+  const [colOffset, setColOffset] = useState(0);
+  const [height, setheight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [clicks, setClicks] = useState(0);
 
-    const [gameGenerated, setGameGenerated] = useState(false);
-    const [board2, setBoard2] = useState<Tile[][]>([]);
-    const [hexCoords, setHexCoords] = useState<number[][]>([]);
+  const [gameGenerated, setGameGenerated] = useState(false);
+  const board = useRef<Tile[][]>([]);
+  const [hexCoords, setHexCoords] = useState<number[][]>([]);
 
-    // const hexCoords = [];
-    // for (let i = 0; i < height; i++) {
-    //     for (let j = 0; j < width; j++) {
-    //         hexCoords.push([i, j]);
-    //     }
-    // }
+  function generateBoard() {
+    const boardDetails = blankBoard();
 
-    //const rowOffset = _.max(hexCoords.filter((x) => x[0] < 0).map((x) => Math.abs(x[0]))) ?? 0;
-    //const colOffset = _.max(hexCoords.filter((x) => x[1] < 0).map((x) => Math.abs(x[1]))) ?? 0;
+    setGameGenerated(true);
+    board.current = boardDetails.board;
+    setRowOffset(boardDetails.rowOffset);
+    setColOffset(boardDetails.colOffset);
+    setWidth(boardDetails.width);
+    setheight(boardDetails.height);
+    setHexCoords(boardDetails.hexCoords);
+  }
 
-    function generateBoard() {
-        const boardDetails = blankBoard();
+  function getAdjacentValues(board: Tile[][], coords: number[]) {
+    const boardHeight = board.length;
+    const boardWidth = board[0].length;
+    const adjTransforms = [
+      [
+        [0, 1],
+        [-1, 1],
+        [-1, 0],
+        [0, -1],
+        [1, 0],
+        [1, 1],
+      ],
+      // odd rows
+      [
+        [0, 1],
+        [-1, 0],
+        [-1, -1],
+        [0, -1],
+        [1, -1],
+        [1, 0],
+      ],
+    ];
+    const relevantTransforms = adjTransforms[Math.abs(coords[0]) % 2];
+    const adjCoords = relevantTransforms
+      .map((mod) => [coords[0] + mod[0], coords[1] + mod[1]])
+      .filter((coord) => coord[0] >= 0 && coord[0] < boardHeight && coord[1] >= 0 && coord[1] < boardWidth);
 
-        setGameGenerated(true);
-        setBoard2(boardDetails.board);
-        setRowOffset(boardDetails.rowOffset);
-        setColOffset(boardDetails.colOffset);
-        setWidth(boardDetails.width);
-        setheight(boardDetails.height);
-        setHexCoords(boardDetails.hexCoords);
+    return adjCoords.map((coord) => ({ i: coord[0], j: coord[1], tile: board[coord[0]][coord[1]] }));
+  }
+
+  const outers = ["ot", "otr", "obr", "ob", "obl", "otl"];
+  const adjTransforms = [
+    [
+      [-2, 0],
+      [-1, 1],
+      [1, 1],
+      [2, 0],
+      [1, 0],
+      [-1, 0],
+    ],
+    [
+      [-2, 0],
+      [-1, 0],
+      [1, 0],
+      [2, 0],
+      [1, -1],
+      [-1, -1],
+    ],
+  ];
+
+  function handleClick(e: MouseEvent, i: number, j: number, seg: string) {
+    e.preventDefault();
+    const clickedConnectionState = board.current[i][j].connections.c.get(seg);
+
+    if (
+      clickedConnectionState === undefined ||
+      clickedConnectionState === ConnectionState.Connected ||
+      clickedConnectionState === ConnectionState.Invalid
+    ) {
+      return;
+    }
+    console.log(i + " " + j + " " + seg);
+    board.current[i][j].connections.c.set(seg, ConnectionState.Connected);
+
+    const indexOfSeg = outers.indexOf(seg);
+    if (indexOfSeg > -1) {
+      const coordTransform = adjTransforms[i%2][indexOfSeg];
+      console.log(coordTransform);
+      console.log(i + coordTransform[0] + " " + (j + coordTransform[1]) + " " + outers[(indexOfSeg + 3) % 6]);
+      board.current[i + coordTransform[0]][j + coordTransform[1]].connections.c.set(
+        outers[(indexOfSeg + 3) % 6],
+        ConnectionState.Connected
+      );
     }
 
-    // function shuffle<T>(array: T[]) {
-    //     for (let i = array.length - 1; i > 0; i--) {
-    //         let j = Math.floor(Math.random() * (i + 1));
-    //         [array[i], array[j]] = [array[j], array[i]];
-    //     }
-    //     return array;
-    // }
+    setClicks(clicks + 1);
+  }
 
-    // function getAdjacentValues(board: Tile[][], coords: number[]) {
-    //     const boardHeight = board.length;
-    //     const boardWidth = board[0].length;
-    //     const adjTransforms = [
-    //         [
-    //             [0, 1],
-    //             [-1, 1],
-    //             [-1, 0],
-    //             [0, -1],
-    //             [1, 0],
-    //             [1, 1],
-    //         ],
-    //         // odd rows
-    //         [
-    //             [0, 1],
-    //             [-1, 0],
-    //             [-1, -1],
-    //             [0, -1],
-    //             [1, -1],
-    //             [1, 0],
-    //         ],
-    //     ];
-    //     const relevantTransforms = adjTransforms[Math.abs(coords[0]) % 2];
-    //     const adjCoords = relevantTransforms
-    //         .map((mod) => [coords[0] + mod[0], coords[1] + mod[1]])
-    //         .filter((coord) => coord[0] >= 0 && coord[0] < boardHeight && coord[1] >= 0 && coord[1] < boardWidth);
+  // function leftClickTile(i: number, j: number) {
+  //     const tile = board.current[i][j];
+  //     if (tile.hasFlag || tile.hasQ) {
+  //         return;
+  //     }
+  //     if (tile.value === -1) {
+  //         board.current[i][j].isRevealed = true;
+  //         if (clicks === 0) {
+  //             setShowFail(true);
+  //         }
+  //         setIsGameFinished(true);
+  //         setIsLost(true);
+  //     } else {
+  //         openSquares([i, j]);
+  //     }
+  // }
 
-    //     return adjCoords.map((coord) => ({ i: coord[0], j: coord[1], tile: board[coord[0]][coord[1]] }));
-    // }
+  // function openSquares(startCoord: number[]) {
+  //     const visited: string[] = [];
+  //     const coordStack = [startCoord];
 
-    // function handleClick(e: MouseEvent<HTMLElement>, i: number, j: number) {
-    //     e.preventDefault();
-    //     if (isGameFinished) return;
-    //     if (e.nativeEvent.button === 0) {
-    //         leftClickTile(i, j);
-    //     } else {
-    //         rightClickTile(i, j);
-    //     }
-    //     if (
-    //         board.current
-    //             .flat()
-    //             .filter((t) => t.value !== -1)
-    //             .every((t) => t.isRevealed)
-    //     ) {
-    //         setShow(true);
-    //         setIsGameFinished(true);
-    //     }
-    //     setClicks(clicks + 1);
-    // }
+  //     while (coordStack.length) {
+  //         let currentCoord = coordStack.pop();
 
-    // function leftClickTile(i: number, j: number) {
-    //     const tile = board.current[i][j];
-    //     if (tile.hasFlag || tile.hasQ) {
-    //         return;
-    //     }
-    //     if (tile.value === -1) {
-    //         board.current[i][j].isRevealed = true;
-    //         if (clicks === 0) {
-    //             setShowFail(true);
-    //         }
-    //         setIsGameFinished(true);
-    //         setIsLost(true);
-    //     } else {
-    //         openSquares([i, j]);
-    //     }
-    // }
+  //         if (currentCoord !== undefined) {
+  //             const stringCoord = `${currentCoord[0]}-${currentCoord[1]}`;
+  //             if (!visited.includes(stringCoord)) {
+  //                 visited.push(stringCoord);
+  //                 board.current[currentCoord[0]][currentCoord[1]].isRevealed = true;
 
-    // function openSquares(startCoord: number[]) {
-    //     const visited: string[] = [];
-    //     const coordStack = [startCoord];
+  //                 if (board.current[currentCoord[0]][currentCoord[1]].value === 0) {
+  //                     const adjs = getAdjacentValues(board.current, currentCoord);
+  //                     coordStack.push(...adjs.map((adj) => [adj.i, adj.j]));
+  //                 }
+  //             }
+  //         }
+  //     }
+  // }
 
-    //     while (coordStack.length) {
-    //         let currentCoord = coordStack.pop();
+  // function rightClickTile(i: number, j: number) {
+  //     const tile = board.current[i][j];
+  //     if (tile.hasFlag) {
+  //         board.current[i][j].hasFlag = false;
+  //         board.current[i][j].hasQ = true;
+  //     } else if (tile.hasQ) {
+  //         board.current[i][j].hasFlag = false;
+  //         board.current[i][j].hasQ = false;
+  //     } else {
+  //         board.current[i][j].hasFlag = true;
+  //         board.current[i][j].hasQ = false;
+  //     }
+  // }
 
-    //         if (currentCoord !== undefined) {
-    //             const stringCoord = `${currentCoord[0]}-${currentCoord[1]}`;
-    //             if (!visited.includes(stringCoord)) {
-    //                 visited.push(stringCoord);
-    //                 board.current[currentCoord[0]][currentCoord[1]].isRevealed = true;
+  // const buttonsDisabled = !(
+  //     controlIsValid(widthString) &&
+  //     controlIsValid(heightString) &&
+  //     controlIsValid(mineCountString) &&
+  //     mineCount <= width * height &&
+  //     (!gameGenerated || isGameFinished)
+  // );
 
-    //                 if (board.current[currentCoord[0]][currentCoord[1]].value === 0) {
-    //                     const adjs = getAdjacentValues(board.current, currentCoord);
-    //                     coordStack.push(...adjs.map((adj) => [adj.i, adj.j]));
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+  // function hideToast() {
+  //     setShowFail(false);
+  //     setShow(false);
+  // }
 
-    // function rightClickTile(i: number, j: number) {
-    //     const tile = board.current[i][j];
-    //     if (tile.hasFlag) {
-    //         board.current[i][j].hasFlag = false;
-    //         board.current[i][j].hasQ = true;
-    //     } else if (tile.hasQ) {
-    //         board.current[i][j].hasFlag = false;
-    //         board.current[i][j].hasQ = false;
-    //     } else {
-    //         board.current[i][j].hasFlag = true;
-    //         board.current[i][j].hasQ = false;
-    //     }
-    // }
+  const radius = 80;
+  const hexContainerStyle = { height: radius * height, width: radius * width * 2.9 };
 
-    // const buttonsDisabled = !(
-    //     controlIsValid(widthString) &&
-    //     controlIsValid(heightString) &&
-    //     controlIsValid(mineCountString) &&
-    //     mineCount <= width * height &&
-    //     (!gameGenerated || isGameFinished)
-    // );
+  return (
+    <Container>
+      {gameGenerated ? (
+        <div className="hex-container" style={hexContainerStyle}>
+          {hexCoords.map((x) => {
+            const tile = board.current[x[0]][x[1]];
 
-    // function hideToast() {
-    //     setShowFail(false);
-    //     setShow(false);
-    // }
+            if (!tile) return;
 
-    const radius = 70;
-    const hexContainerStyle = { height: radius * (height-1), width: radius * width * 2.9 };
-
-    return (
-        <Container>
-            <Button
-                variant="secondary"
-                className="ms-2"
-                onClick={generateBoard}
-            >
-                Reset Board
-            </Button>
-            {gameGenerated ? (<div className="hex-container" style={hexContainerStyle}>
-                {hexCoords.map((x) => {
-                    const tile = board2[x[0]][x[1]];
-
-                    if (!tile) return;
-
-                    return (
-                        <Hex key={`pz${x[0]}-${x[1]}`}
-                            row={x[0]}
-                            column={x[1]}
-                            rowOffset={rowOffset}
-                            columnOffset={colOffset}
-                            radius={radius}
-                            connections={tile.connections} />
-                    );
-                })}
-            </div>) : null}
-        </Container>
-    );
+            return (
+              <Hex
+                key={`pz${x[0]}-${x[1]}`}
+                row={x[0]}
+                column={x[1]}
+                rowOffset={rowOffset}
+                columnOffset={colOffset}
+                radius={radius}
+                connections={tile.connections}
+                onClick={handleClick}
+              />
+            );
+          })}
+        </div>
+      ) : null}
+      <Button variant="secondary" className="ms-2" onClick={generateBoard}>
+        Reset Board
+      </Button>
+    </Container>
+  );
 }
