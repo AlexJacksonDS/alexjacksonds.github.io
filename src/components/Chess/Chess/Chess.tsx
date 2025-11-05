e132fe9e64b502d508ece95e0c30f82bb3e3a188
AlexJacksonDS <alexander.jackson@dorsetsoftware.com>
2025-11-05 11:41:29 +0000

    Refactor Chess SignalR

--- src/components/Chess/Chess/Chess.tsx
+++ src/components/Chess/Chess/Chess.tsx
@@ -1,21 +1,14 @@
 "use client";
 
 import { Chess as ChessJS, Square } from "chess.js";
-import { useContext, useEffect, useRef, useState, KeyboardEvent } from "react";
+import { useState, KeyboardEvent } from "react";
 import { Col, Container, FormGroup, FormLabel, Row } from "react-bootstrap";
 import ChessBoard from "../ChessBoard/ChessBoard";
-import { useRouter } from "next/navigation";
 import "./Chess.scss";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 import { getPieceUnicodeFromString } from "@/helpers/pieceUnicodeHelper";
-import { UserContext } from "@/app/UserContext";
+import useSignalR from "@/hooks/useSignalR";
 
 export default function Chess() {
-  const [isInit, setIsInit] = useState(false);
-  const userData = useContext(UserContext);
-  const router = useRouter();
-  const connectionRef = useRef<HubConnection | undefined>();
-
   const [gameId, setGameId] = useState("");
   const [gameIdDisabled, setGameIdDisabled] = useState(false);
   const [connectedToGame, setConnectedToGame] = useState(false);
@@ -30,66 +23,37 @@ export default function Chess() {
   const isStaleMate = chess.isStalemate();
   const isCheckMate = chess.isCheckmate();
 
-  useEffect(() => {
-    if (userData.isReady && !userData.token) {
-      router.push("/");
-    }
+  const signalRConnection = useSignalR("fen", [
+    ["joinFailed", joinFailed],
+    ["fen", handleFen],
+  ]);
 
-    if (!isInit) {
-      if (userData.isLoggedIn && userData.token && userData.accessTokenExpiry) {
-        if (!connectionRef.current) {
-          connectionRef.current = new HubConnectionBuilder()
-            .withUrl(`${process.env.NEXT_PUBLIC_API}/fen`, {
-              withCredentials: false,
-              accessTokenFactory: async () => getToken(),
-            })
-            .build();
-
-          connectionRef.current.on("joinFailed", () => {
-            setGameId("");
-            setGameIdDisabled(false);
-          });
-
-          connectionRef.current.on("fen", (fen: string) => {
-            handleFen(fen);
-          });
-
-          connectionRef.current.start().catch((err) => console.log(err));
-          setIsInit(true);
-        }
-      }
-    }
-  }, [userData, isInit, router, gameId]);
-
-  async function getToken() {
-    if (userData.accessTokenExpiry < Math.floor(new Date().getTime() / 1000)) {
-      const newToken = await userData.refresh();
-      return newToken;
-    }
-    return userData.token ?? "";
+  function joinFailed() {
+    setGameId("");
+    setGameIdDisabled(false);
   }
 
   const gameIdOnKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key != "Enter") return;
-    if (connectionRef.current) {
-      connectionRef.current.send("joinGame", gameId, chess.pgn()).then(() => {
+    if (signalRConnection) {
+      signalRConnection.send("joinGame", gameId, chess.pgn()).then(() => {
         setGameIdDisabled(true);
         setConnectedToGame(true);
       });
     }
   };
 
-  const handleFen = (fen: string) => {
+  function handleFen(fen: string) {
     const newChess = new ChessJS();
     newChess.loadPgn(fen);
     setChessJS(newChess);
-  };
+  }
 
   function handleClick(clickedSquare?: Square) {
     if (isCheckMate || isDraw || isStaleMate || !clickedSquare) {
       return;
     }
-    if (connectionRef.current) {
+    if (signalRConnection) {
       if (selectedSquare && possibleMoves && clickedSquare !== selectedSquare) {
         if (
           !possibleMoves.find((pm) => pm.includes(clickedSquare)) &&
@@ -102,7 +66,11 @@ export default function Chess() {
         if (isPossiblePromotion(selectedSquare)) {
           var promotion = getPromotionOption();
           if (promotion) {
-            chess.move({ from: selectedSquare, to: clickedSquare, promotion: promotion });
+            chess.move({
+              from: selectedSquare,
+              to: clickedSquare,
+              promotion: promotion,
+            });
           }
         } else {
           chess.move({ from: selectedSquare, to: clickedSquare });
@@ -111,7 +79,7 @@ export default function Chess() {
         setPossibleMoves([]);
         setMyColour(getMyColour());
 
-        connectionRef.current.send("takeTurn", gameId, chess.pgn());
+        signalRConnection.send("takeTurn", gameId, chess.pgn());
       } else {
         var piece = chess.get(clickedSquare);
         if (piece && piece.color === chess.turn() && isTurn(piece.color)) {
@@ -123,7 +91,11 @@ export default function Chess() {
     }
   }
 
-  function isCastle(squareCode: string, possibleMoves: string[], selectedSquare?: string) {
+  function isCastle(
+    squareCode: string,
+    possibleMoves: string[],
+    selectedSquare?: string
+  ) {
     if (selectedSquare === "e1") {
       if (squareCode === "g1") {
         return possibleMoves.includes("O-O");
@@ -159,15 +131,21 @@ export default function Chess() {
   function isPossiblePromotion(selectedSquare: Square) {
     var piece = chess.get(selectedSquare);
     return (
-      (piece.type === "p" && piece.color === "b" && selectedSquare.charAt(1) === "2") ||
-      (piece.type === "p" && piece.color === "w" && selectedSquare.charAt(1) === "7")
+      (piece.type === "p" &&
+        piece.color === "b" &&
+        selectedSquare.charAt(1) === "2") ||
+      (piece.type === "p" &&
+        piece.color === "w" &&
+        selectedSquare.charAt(1) === "7")
     );
   }
 
   function getPromotionOption() {
     var piece = null;
     while (piece == null) {
-      var input = prompt("Enter a piece to promote to: q = Queen, r = Rook, b = Bishop, n = Knight");
+      var input = prompt(
+        "Enter a piece to promote to: q = Queen, r = Rook, b = Bishop, n = Knight"
+      );
       if (["q", "r", "b", "n"].includes(input ?? "")) {
         piece = input;
       }
@@ -211,7 +189,9 @@ export default function Chess() {
       <Row className="g-0-bottom">
         <Col className="g-0">
           <Container className="faux-borders-extra-thin">
-            <Container className="captured-pieces w dark">{blackCaptures}</Container>
+            <Container className="captured-pieces w dark">
+              {blackCaptures}
+            </Container>
           </Container>
         </Col>
         <Col className="g-0">
@@ -222,24 +202,34 @@ export default function Chess() {
               selectedSquare={selectedSquare}
               possibleMoves={possibleMoves}
               previousMove={
-                chess.history({ verbose: true }).length > 0 ? chess.history({ verbose: true }).at(-1)?.lan : ""
+                chess.history({ verbose: true }).length > 0
+                  ? chess.history({ verbose: true }).at(-1)?.lan
+                  : ""
               }
             />
           </Container>
         </Col>
         <Col className="g-0">
           <Container className="faux-borders-extra-thin">
-            <Container className="captured-pieces b light bottom">{whiteCaptures}</Container>
+            <Container className="captured-pieces b light bottom">
+              {whiteCaptures}
+            </Container>
           </Container>
         </Col>
       </Row>
       <Row className="black-background g-0-top text-center">
         <Col>
           {!isCheckMate && !isDraw && !isStaleMate ? (
-            <p>{`It is currently ${chess.turn() === "w" ? "White" : "Black"}'s turn`}</p>
+            <p>{`It is currently ${
+              chess.turn() === "w" ? "White" : "Black"
+            }'s turn`}</p>
           ) : null}
-          {isCheckMate ? <p>{`${chess.turn() === "w" ? "Black" : "White"} wins`}</p> : null}
-          {isCheck && !isCheckMate ? <p>{`${chess.turn() === "w" ? "White" : "Black"} is in check`}</p> : null}
+          {isCheckMate ? (
+            <p>{`${chess.turn() === "w" ? "Black" : "White"} wins`}</p>
+          ) : null}
+          {isCheck && !isCheckMate ? (
+            <p>{`${chess.turn() === "w" ? "White" : "Black"} is in check`}</p>
+          ) : null}
           {isDraw ? <p>Game ended in draw</p> : null}
           {isStaleMate ? <p>Game ended in stalemate</p> : null}
         </Col>
