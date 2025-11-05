8c72c56bc02373c271331660c16be5097735458b
AlexJacksonDS <alexander.jackson@dorsetsoftware.com>
2025-11-05 11:44:30 +0000

    Refactor Othello SignalR

--- src/components/Othello/Othello/Othello.tsx
+++ src/components/Othello/Othello/Othello.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useContext, useEffect, useRef, useState, KeyboardEvent } from "react";
+import { useState, KeyboardEvent } from "react";
 import { Col, Container, FormGroup, FormLabel, Row } from "react-bootstrap";
-import { useRouter } from "next/navigation";
 import OthelloBoard from "..//OthelloBoard/OthelloBoard";
 import { isArrayInArray } from "../../../helpers/arrayHelper";
 import {
@@ -14,15 +13,9 @@ import {
   performMove,
 } from "../../../services/othello.service";
 import { OthelloBoard as Board, OthelloTurn } from "../../../types/othello";
-import { UserContext } from "@/app/UserContext";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import useSignalR from "@/hooks/useSignalR";
 
 export default function Othello() {
-  const [isInit, setIsInit] = useState(false);
-  const userData = useContext(UserContext);
-  const router = useRouter();
-  const connectionRef = useRef<HubConnection | undefined>();
-
   const [gameId, setGameId] = useState("");
   const [gameIdDisabled, setGameIdDisabled] = useState(false);
   const [connectedToGame, setConnectedToGame] = useState(false);
@@ -32,65 +25,47 @@ export default function Othello() {
   const [currentTurn, setCurrentTurn] = useState<OthelloTurn>(1);
   const boardStatus = boardState(board);
 
-  useEffect(() => {
-    if (userData.isReady && !userData.token) {
-      router.push("/");
-    }
+  const signalRConnection = useSignalR("fen", [
+    ["joinFailed", joinFailed],
+    ["fen", handleFen],
+  ]);
 
-    if (!isInit) {
-      if (userData.isLoggedIn && userData.token && userData.accessTokenExpiry) {
-        if (!connectionRef.current) {
-          connectionRef.current = new HubConnectionBuilder()
-            .withUrl(`${process.env.NEXT_PUBLIC_API}/fen`, {
-              withCredentials: false,
-              accessTokenFactory: async () => getToken(),
-            })
-            .build();
-
-          connectionRef.current.on("joinFailed", () => {
-            setGameId("");
-            setGameIdDisabled(false);
-          });
-
-          connectionRef.current.on("fen", (fen: string) => {
-            handleFen(fen);
-          });
-
-          connectionRef.current.start().catch((err) => console.log(err));
-          setIsInit(true);
-        }
-      }
-    }
-  }, [userData, isInit, router, gameId]);
-
-  async function getToken() {
-    if (userData.accessTokenExpiry < Math.floor(new Date().getTime() / 1000)) {
-      const newToken = await userData.refresh();
-      return newToken;
-    }
-    return userData.token ?? "";
+  function joinFailed() {
+    setGameId("");
+    setGameIdDisabled(false);
   }
 
-  const handleFen = (fen: string) => {
+  function handleFen(fen: string) {
     const split = fen.split(" ");
     setCurrentTurn(split[1] === "1" ? 2 : 1);
     setMyColour(split[1] === "1" ? 2 : 1);
     setBoard(boardFromString(split[0]));
-  };
+  }
 
   function handleClick(i: number, j: number) {
-    if (connectionRef.current) {
-      if (!boardStatus.isBoardFull && (currentTurn === myColour || myColour === undefined)) {
+    if (signalRConnection) {
+      if (
+        !boardStatus.isBoardFull &&
+        (currentTurn === myColour || myColour === undefined)
+      ) {
         var possibleMoves = getPossibleMoves(board, currentTurn);
         if (isArrayInArray(possibleMoves, [i, j])) {
           const tempBoard = performMove(board, i, j, currentTurn);
           setBoard(tempBoard);
           setCurrentTurn(
-            getPossibleMoves(board, currentTurn === 1 ? 2 : 1).length > 0 ? (currentTurn === 1 ? 2 : 1) : currentTurn
+            getPossibleMoves(board, currentTurn === 1 ? 2 : 1).length > 0
+              ? currentTurn === 1
+                ? 2
+                : 1
+              : currentTurn
           );
           setMyColour(currentTurn);
 
-          connectionRef.current.send("takeTurn", gameId, `${boardToString(tempBoard)} ${currentTurn}`);
+          signalRConnection.send(
+            "takeTurn",
+            gameId,
+            `${boardToString(tempBoard)} ${currentTurn}`
+          );
         }
       }
     }
@@ -98,11 +73,13 @@ export default function Othello() {
 
   const gameIdOnKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key != "Enter") return;
-    if (connectionRef.current) {
-      connectionRef.current.send("joinGame", gameId, boardToString(board)).then(() => {
-        setGameIdDisabled(true);
-        setConnectedToGame(true);
-      });
+    if (signalRConnection) {
+      signalRConnection
+        .send("joinGame", gameId, boardToString(board))
+        .then(() => {
+          setGameIdDisabled(true);
+          setConnectedToGame(true);
+        });
     }
   };
 
@@ -117,7 +94,9 @@ export default function Othello() {
                 <input
                   className="form-control"
                   value={gameId}
-                  onInput={(e) => setGameId((e.target as HTMLInputElement).value)}
+                  onInput={(e) =>
+                    setGameId((e.target as HTMLInputElement).value)
+                  }
                   onKeyUp={(e) => gameIdOnKeyUp(e)}
                   disabled={gameIdDisabled}
                   placeholder="Enter to submit"
@@ -139,7 +118,9 @@ export default function Othello() {
         </Row>
         <Row className="black-background g-0-top text-center">
           <Col>
-            {!boardStatus.isBoardFull ? <div>Current turn: {currentTurn === 1 ? "White" : "Black"}</div> : null}
+            {!boardStatus.isBoardFull ? (
+              <div>Current turn: {currentTurn === 1 ? "White" : "Black"}</div>
+            ) : null}
             <div>White score: {boardStatus.whiteScore}</div>
             <div>Black score: {boardStatus.blackScore}</div>
             {boardStatus.isBoardFull ? (
